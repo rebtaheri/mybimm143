645f85673db6adb65ecd1bc5a1ed966a8677af3c
rebtaheri <rtaheri@ucsd.edu>
2026-03-12 10:45:22 -0700

    Add files via upload

---
+++ class04/class04.r
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Rebecca Taheri"
+#' date: "Janurary 15th, 2026"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col= "purple", lwd=2)
+log(10, base = 10)
